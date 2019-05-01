b514340bc4cc11b953ca04ff92b360843edc4108
bawillso <31483951+bawillso@users.noreply.github.com>
2019-04-30 21:15:08 -0400

    Update success.php

--- success.php
+++ success.php
@@ -6,3 +6,6 @@ session_start();
 ?>
 
 success!
+
+<a href='http://csis.svsu.edu/~bawillso/Prog3/customer.php' target='_blank'>Go Home</a><br />
+<a href='http://csis.svsu.edu/~bawillso/Prog3/logout.php' target='_blank'>Logout</a><br />
